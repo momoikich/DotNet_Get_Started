8d7a4bd9ca018d03e68feabf7884755713ccde18
momoikich <mohamed.ikich@etu.toulouse-inp.fr>
2023-12-21 12:49:02 +0100

    add a project (.NET desktop app for calculation )

---
+++ MaPremiereApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bonjour, ceci est ma première application .NET !");
+    }
+}
+
